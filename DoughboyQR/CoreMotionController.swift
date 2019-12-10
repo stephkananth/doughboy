982ffd5e4e9d2c98a139e5ff4fa24d5b6032aaa1
Stephanie Ananth <stephkananth@gmail.com>
2019-12-10 16:02:29 -0500

    initial commit; todo: coremotion

---
+++ DoughboyQR/CoreMotionController.swift
@@ -0,0 +1,20 @@
+//
+//  CoreMotionController.swift
+//  QRCodeReader
+//
+//  Created by Steph Ananth on 12/10/19.
+//  Copyright © 2019 AppCoda. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import CoreMotion
+
+class CoreMotionController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationItem.hidesBackButton = true
+    }
+    
+}
