37d0bac78567a8e120c677a03773f42c69ba312d
Stephanie Ananth <stephkananth@gmail.com>
2019-12-11 11:06:11 -0500

    all functionality works; todo: testing and logic

---
+++ DoughboyQR/View Controllers/DoneViewController.swift
@@ -0,0 +1,20 @@
+//
+//  DoneViewController.swift
+//  DoughboyQR
+//
+//  Created by Steph Ananth on 12/11/19.
+//  Copyright © 2019 AppCoda. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class DoneViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    self.title = "Done!"
+    self.navigationItem.hidesBackButton = true
+  }
+  
+}
--- DoughboyQR/View Controllers/View Controllers/CoreMotionController.swift
+++ DoughboyQR/View Controllers/View Controllers/CoreMotionController.swift
@@ -12,9 +12,44 @@ import CoreMotion
 
 class CoreMotionController: UIViewController {
   
+  let motionManager = CMMotionManager()
+  var timer: Timer!
+  
+  let threshold: Double = 0.5
+  let target = Int.random(in: 1..<3)
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     self.navigationItem.hidesBackButton = true
+    
+    switch self.target {
+    case 1:
+      self.title = "<-- Left"
+    case 2:
+      self.title = "Right -->"
+    default:
+      self.title = "ERROR"
+    }
+    
+    motionManager.startAccelerometerUpdates()
+    timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(CoreMotionController.update), userInfo: nil, repeats: true)
+  }
+  
+  @objc func update() {
+    if let accelerometerData = motionManager.accelerometerData {
+      switch self.target {
+      case 1:
+        if accelerometerData.acceleration.x <= -self.threshold, let _: CoreMotionController = self.navigationController?.viewControllers.last as? CoreMotionController {
+          performSegue(withIdentifier: "tiltConfirmed", sender: self)
+        }
+      case 2:
+        if accelerometerData.acceleration.x >= self.threshold, let _: CoreMotionController = self.navigationController?.viewControllers.last as? CoreMotionController{
+          performSegue(withIdentifier: "tiltConfirmed", sender: self)
+        }
+      default:
+        print("ERROR")
+      }
+    }
   }
   
 }
